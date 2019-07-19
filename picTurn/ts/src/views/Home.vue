b787425c74243981a4b2e6493f2394928f6a6615
luduteng <luduteng@sina.com>
2019-07-19 17:06:17 +0800

    ts

---
+++ picTurn/ts/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+   <pic-turn></pic-turn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import PicTurn from '@/components/picTurn.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    PicTurn,
+  },
+})
+export default class Home extends Vue {}
+</script>
